af262229a2245fd7add8e22456606c99ff66413c
fu013 <fu017@naver.com>
2023-11-24 12:45:55 +0900

    deploy: 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -6,35 +6,35 @@ PROJECT_NAME=_scarlet
 cd $REPOSITORY
 
 echo "가상 메모리 초기화"
-sudo swapoff -v /mnt/swapfile
-sudo rm /mnt/swapfile
+swapoff -v /mnt/swapfile
+rm /mnt/swapfile
 
 echo "가상 메모리 설정(2GB)"
-sudo dd if=/dev/zero of=/mnt/swapfile bs=1M count=2048
-sudo mkswap /mnt/swapfile
-sudo swapon /mnt/swapfile
+dd if=/dev/zero of=/mnt/swapfile bs=1M count=2048
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
 
 echo "메모리 조회"
-sudo free -h
+free -h
 
 echo "디스크 조회"
-sudo df -h
+df -h
 
 echo "사용 포트 초기화"
-sudo kill `sudo lsof -t -i:3306`
-sudo kill `sudo lsof -t -i:9999`
+kill `lsof -t -i:3306`
+kill `lsof -t -i:9999`
 
 echo "Docker 컨테이너&이미지 삭제(용량 비우기)"
-sudo docker rmi -f $(docker images -q)
-sudo docker rm -f $(docker ps -qa)
-sudo killall containerd-shim
+docker rmi -f $(docker images -q)
+docker rm -f $(docker ps -qa)
+killall containerd-shim
 
 echo "Docker 네트워크 삭제"
-sudo docker network rm -f $(docker network ls -q)
+docker network rm -f $(docker network ls -q)
 
 echo "Docker 컨테이너 로그&볼륨 삭제(용량 비우기)"
-sudo docker system prune -af
-sudo docker volume rm $(docker volume ls -qf dangling=true)
+docker system prune -af
+docker volume rm $(docker volume ls -qf dangling=true)
 
 echo "도커 컨테이너 배포"
-sudo docker compose up -d
+docker compose up -d
